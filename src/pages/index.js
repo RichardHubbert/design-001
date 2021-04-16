e049bba04a0ba75345d794fa48065ab5ebcf4232
Richard Hubbert <richardhubbert@gmail.com>
2021-04-16 18:39:18 +0100

    160421 001 Images and Styles assed

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,14 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi Hilary Hubbert</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>We Design and Develop UI | UX</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Watch the Video</Link> <br />      
+      </div>
+    </div>
   </Layout>
 )
 
