7d6866dea5177e49b5721e40881d5efdb0905381
Richard Hubbert <richardhubbert@gmail.com>
2021-04-18 07:02:22 +0100

    180421 Style and Structure

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,17 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import logo from '../images/logo-designcode.svg'
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
-      <Link to="/"><img src="images/wallpaper2.jpg" width="30" />Header Link</Link>
+      <Link to="/"><img src={logo}  width="" /></Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/dowmloads">Downloads</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/buy"><button>Buy</button></Link>
     </div>
   </div>
 )
