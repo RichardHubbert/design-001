8b65e3cf3dde52cf2bb41b694e7af240ebdf76b7
Richard Hubbert <richardhubbert@gmail.com>
2021-04-20 08:09:09 +0100

    200421 Styles

---
+++ src/components/Section.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = props => {
+    <SectionGroup>
+        <SectionLogo>
+        <SectionTitleGroup>
+
+        </SectionTitleGroup>
+    </SectionGroup>
+}
+
+
+
+
+export default Section
+
+
+
